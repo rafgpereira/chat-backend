d6c8163195b70b2060c34580445aa45d6433a087
rafgpereira <rafaelgomespereira123@gmail.com>
2024-01-30 16:54:06 -0300

    chat

---
+++ index.js
@@ -0,0 +1,29 @@
+const app = require("express");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "http://localhost:3000" },
+});
+
+const PORT = 3003;
+
+io.on("connection", (socket) => {
+  socket.on("set_username", (username) => {
+    socket.data.username = username;
+  });
+
+  console.log("Usuario conectado", socket.data.id);
+
+  socket.on("disconnect", (reason) => {
+    console.log("Usuário desconectado", socket.id);
+  });
+
+  socket.on("message", (text) => {
+    io.emit("receive_message", {
+      text,
+      authorId: socket.id,
+      author: socket.data.username,
+    });
+  });
+});
+
+server.listen(PORT, () => console.log("Servidor conectado."));

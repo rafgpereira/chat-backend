9f29c40fa08335b88697eb240a4ca585895dfa75
rafgpereira <rafaelgomespereira123@gmail.com>
2024-01-30 16:55:27 -0300

    url update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express");
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  cors: { origin: "http://localhost:3000" },
+  cors: { origin: "https://user-login-system.netlify.app" },
 });
 
 const PORT = 3003;
